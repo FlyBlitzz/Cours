a50bddc7e4625f196b9c601e7b9d2ad5de2f7f1a
unknown <VILLARDN@SIOPERGAUD.FR>
2025-12-09 17:54:52 +0100

    Cours

---
+++ Développement/PHP/Projets/PHP-INTRO/fonctions/cours.php
@@ -0,0 +1,12 @@
+<?php
+
+// Définir une fonction qui affiche "Bonjour"
+function direBonjour(): void
+{
+    echo "Bonjour \n";
+}
+
+// Appeler/Utiliser la fonction
+direBonjour();
+
+direBonjour();
